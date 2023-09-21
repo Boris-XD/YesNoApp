cbe965608759134f8ae7410d545ac6ad975aef61
Boris Sea <seaborisrc@gmail.com>
2023-09-21 19:56:27 -0400

    YesNoApp - Refactor text message

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -6,18 +6,15 @@ class MessageFieldBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final underlineInputBorder = UnderlineInputBorder(
+      borderRadius: BorderRadius.circular(40),
+      borderSide: const BorderSide(color: Colors.transparent),
+    );
 
-
-    return TextFormField(
-      decoration: InputDecoration(
-        enabledBorder: UnderlineInputBorder(
-          borderRadius: BorderRadius.circular(40),
-          borderSide: const BorderSide(color: Colors.transparent),
-        ),
-        focusedBorder: UnderlineInputBorder(
-          borderRadius: BorderRadius.circular(40),
-          borderSide: const BorderSide(color: Colors.transparent),
-        ),
+    final inputDecoration = InputDecoration(
+        hintText: 'End your message with a?',
+        enabledBorder: underlineInputBorder,
+        focusedBorder: underlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
           icon: const Icon(Icons.send),
@@ -25,7 +22,10 @@ class MessageFieldBox extends StatelessWidget {
             print('Send Submited');
           },
         ),
-      ),
+      );
+
+    return TextFormField(
+      decoration: inputDecoration,
       onFieldSubmitted: (value) => print('Send Submited $value'),
       onChanged: (value) => print('Send Changed $value'),
     );
